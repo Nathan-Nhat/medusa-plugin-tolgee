75902347079d76a8bbcb94309cff05bec2320e48
ttnhat <trantrungnhat6196@gmail.com>
2025-05-17 04:58:40 +0700

    use backend url

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -1,7 +1,12 @@
 import Medusa from "@medusajs/js-sdk"
+declare global {
+    interface Window {
+        __BACKEND_URL__?: string
+    }
+}
 
 export const sdk = new Medusa({
-    baseUrl: import.meta.env.VITE_BACKEND_URL || "/",
+    baseUrl: window.__BACKEND_URL__ || "/",
     debug: import.meta.env.DEV,
     auth: {
         type: "session",
