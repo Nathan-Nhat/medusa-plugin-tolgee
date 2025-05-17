2b498e4c8c3e2602984b79b9046a99b800cbfe7b
ttnhat <trantrungnhat6196@gmail.com>
2025-05-17 16:00:15 +0700

    change env

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -1,12 +1,7 @@
 import Medusa from "@medusajs/js-sdk"
-declare global {
-    interface Window {
-        __BACKEND_URL__?: string
-    }
-}
 
 export const sdk = new Medusa({
-    baseUrl: window.__BACKEND_URL__ || "/",
+    baseUrl: import.meta.env.VITE_BACKEND_URL || "/",
     debug: import.meta.env.DEV,
     auth: {
         type: "session",
